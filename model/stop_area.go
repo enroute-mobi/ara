d1dc111eb41c131c6a9481f2c45c3811811afd3d
Vincent Marie <vincent.marie@af83.com>
2016-10-05 11:18:26 +0200

    Create MemoryStopAreas and associated tests. Refs #1755

---
+++ model/stop_area.go
@@ -0,0 +1,52 @@
+package model
+
+type StopAreaId int64
+
+type StopArea struct {
+    id StopAreaId
+
+    // Name string
+    // ...
+}
+
+func (stopArea *StopArea) Id() StopAreaId {
+   return stopArea.id
+}
+
+type MemoryStopAreas struct {
+	byIdentifier map[StopAreaId]*StopArea
+	lastIdentifier StopAreaId
+}
+
+func NewMemoryStopAreas() *MemoryStopAreas {
+	return &MemoryStopAreas{
+		byIdentifier: make(map[StopAreaId]*StopArea),
+	}
+}
+
+func (manager *MemoryStopAreas) New() StopArea {
+	return StopArea{}
+}
+
+func (manager *MemoryStopAreas) Find(id StopAreaId) (StopArea, bool) {
+	stopArea, ok := manager.byIdentifier[id]
+	if ok {
+		return *stopArea, true
+	} else {
+		return StopArea{}, false
+	}
+}
+
+func (manager *MemoryStopAreas) Save(stopArea *StopArea) bool {
+	if stopArea.Id() == 0 {
+		stopArea.id = manager.lastIdentifier + 1
+		manager.lastIdentifier = stopArea.id
+	}
+	manager.byIdentifier[stopArea.Id()] = stopArea
+	return true
+}
+
+func (manager *MemoryStopAreas) Delete(stopArea *StopArea) bool {
+	delete(manager.byIdentifier, stopArea.Id())
+	return true
+}
---
+++ model/stop_area_test.go
@@ -0,0 +1,90 @@
+package model
+
+import (
+    "testing"
+)
+
+func Test_StopArea_Id(t *testing.T) {
+	stopArea := StopArea{
+		id: 42,
+	}
+
+	if stopArea.Id() != 42 {
+   	 	t.Errorf("StopArea.Id() returns wrong value, got: %v, required: %v", stopArea.Id(), 42)
+   	}
+}
+
+func Test_MemoryStopAreas_New(t *testing.T) {
+    stopAreas := NewMemoryStopAreas()
+
+    stopArea := stopAreas.New()
+    if stopArea.Id() != 0 {
+        t.Errorf("New StopArea identifier should be zero, got: %d", stopArea.Id())
+    }
+}
+
+func Test_MemoryStopAreas_Save(t *testing.T) {
+    stopAreas := NewMemoryStopAreas()
+
+    stopArea := stopAreas.New()
+
+    if success := stopAreas.Save(&stopArea); !success {
+        t.Errorf("Save should return true")
+    }
+
+    if stopArea.Id() == 0 {
+        t.Errorf("New StopArea identifier shouldn't be zero")
+    }
+}
+
+func Test_MemoryStopAreas_Save_NextIdentifier(t *testing.T) {
+	stopAreas := NewMemoryStopAreas()
+
+	for expectedIdentifier := 1; expectedIdentifier < 10; expectedIdentifier++ {
+		stopArea := stopAreas.New()
+		stopAreas.Save(&stopArea)
+
+		if stopArea.Id() != StopAreaId(expectedIdentifier) {
+	        t.Errorf("New StopArea identifier should be %v, got: %v", expectedIdentifier, stopArea.Id())
+	    }
+	}
+}
+
+func Test_MemoryStopAreas_Find_NotFound(t *testing.T) {
+    stopAreas := NewMemoryStopAreas()
+    _, ok := stopAreas.Find(1)
+    if ok {
+        t.Errorf("Find should return false when StopArea isn't found")
+    }
+}
+
+func Test_MemoryStopAreas_Find(t *testing.T) {
+    stopAreas := NewMemoryStopAreas()
+
+    existingStopArea := stopAreas.New()
+    stopAreas.Save(&existingStopArea)
+
+    stopAreaId := existingStopArea.Id()
+
+    stopArea, ok := stopAreas.Find(stopAreaId)
+    if !ok {
+        t.Errorf("Find should return true when StopArea is found")
+    }
+    if stopArea.Id() != stopAreaId {
+        t.Errorf("Find should return a StopArea with the given Id")
+    }
+}
+
+func Test_MemoryStopAreas_Delete(t *testing.T) {
+	stopAreas := NewMemoryStopAreas()
+
+	existingStopArea := stopAreas.New()
+	stopAreas.Save(&existingStopArea)
+
+	stopAreas.Delete(&existingStopArea)
+
+	_, ok := stopAreas.Find(existingStopArea.Id())
+    if ok {
+        t.Errorf("Deleted StopArea should be findable")
+    }
+}
