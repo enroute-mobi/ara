14763dff89ea9cf81fbea214c177100b4b7ae104
Edouard Maffert <edouard.maffert@af83.com>
2016-12-13 12:26:50 +0100

    disable outdated cucumber test

--- features/step_definitions/siri_steps.rb
+++ features/step_definitions/siri_steps.rb
@@ -1,12 +1,8 @@
-# Given(/^a SIRI HTTP server$/) do
-#   system "go run edwig.go -testuuid -testclock=20170101-1200 api &"
+# Given(/^we send a checkstatus request with body "([^"]+)"$/) do |filename|
+#   system "rm features/testdata/response.xml"
+#   system "curl -s -H 'Content-Type: text/xml' #{$server}/default/siri --data @features/testdata/#{filename} > features/testdata/response.xml"
 # end
 
-Given(/^we send a checkstatus request with body "([^"]+)"$/) do |filename|
-  system "rm features/testdata/response.xml"
-  system "curl -s -H 'Content-Type: text/xml' #{$server}/default/siri --data @features/testdata/#{filename} > features/testdata/response.xml"
-end
-
-Then(/^we should recieve a checkstatus response with body "([^"]+)"$/) do |filename|
-  expect(IO.read("features/testdata/response.xml")).to eq(IO.read("features/testdata/#{filename}"))
-end
+# Then(/^we should recieve a checkstatus response with body "([^"]+)"$/) do |filename|
+#   expect(IO.read("features/testdata/response.xml")).to eq(IO.read("features/testdata/#{filename}"))
+# end
