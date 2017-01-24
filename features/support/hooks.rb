bc51fcb570fcbbb7338bac444d19501b56fc0db3
Alban Peignier <alban.peignier@af83.com>
2017-01-24 09:41:08 +0100

    Set EDWIG_ENV=test when running edwig in cucumber. Send edwig messages to log/edwig.log. Refs #2452

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,7 +6,10 @@ Before('@server') do
   unless File.directory?("tmp")
     FileUtils.mkdir_p("tmp")
   end
-  system "go run edwig.go -pidfile=tmp/pid -testuuid -testclock=20170101-1200 api -listen=localhost:8081 > /dev/null 2>&1 &"
+  unless File.directory?("log")
+    FileUtils.mkdir_p("log")
+  end
+  system "EDWIG_ENV=test go run edwig.go -debug -pidfile=tmp/pid -testuuid -testclock=20170101-1200 api -listen=localhost:8081 >> log/edwig.log 2>&1 &"
 
   time_limit = Time.now + 30
   begin
