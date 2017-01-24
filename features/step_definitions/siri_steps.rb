5c091a11f10c4138216850751da8d263bd7c7a40
Alban Peignier <alban.peignier@af83.com>
2017-01-24 10:12:27 +0100

    Remove unnecessary file to store last siri response

--- features/step_definitions/siri_steps.rb
+++ features/step_definitions/siri_steps.rb
@@ -1,23 +1,19 @@
 require 'rexml/document'
 require 'rexml/xpath'
 
-working_directory = "features/testdata"
-
 def siri_path(attributes = {})
   url_for(attributes.merge(path: "siri"))
 end
 
 Given(/^we send a checkstatus request for referential "([^"]+)"$/) do |referential|
-  system "rm #{working_directory}/response.xml"
-  xmlBody = File.read("#{working_directory}/checkstatus-soap-request.xml")
+  xmlBody = File.read("features/testdata/checkstatus-soap-request.xml")
   response = RestClient.post siri_path(referential: referential), xmlBody, {content_type: :xml}
-  File.write("#{working_directory}/response.xml", response.body)
+  @last_siri_response = response.body
 end
 
 Then(/^we should recieve a positive checkstatus response$/) do
-  xmlBody = File.read("#{working_directory}/response.xml")
+  xmlBody = @last_siri_response
   doc = REXML::Document.new xmlBody
   status = REXML::XPath.first(doc, "//*[local-name()='Status']")
-
   expect(status.text).to eq("true")
 end
