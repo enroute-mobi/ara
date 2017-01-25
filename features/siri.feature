86e3f0f50a9a41c7544f02632bd032436a6ec67d
Alban Peignier <alban.peignier@af83.com>
2017-01-25 11:15:37 +0100

    Fixes typo in features/siri.feature

--- features/siri.feature
+++ features/siri.feature
@@ -1,6 +1,7 @@
 Feature: Siri server
+  Background:
+    Given a Referential "test" is created
 
   Scenario: Handle a SIRI Checkstatus request
-    Given a Referential "test" exists
-      And we send a checkstatus request for referential "test"
-    Then we should recieve a positive checkstatus response
+    When we send a checkstatus request for referential "test"
+    Then we should receive a positive checkstatus response
--- features/step_definitions/siri_steps.rb
+++ features/step_definitions/siri_steps.rb
@@ -5,13 +5,13 @@ def siri_path(attributes = {})
   url_for(attributes.merge(path: "siri"))
 end
 
-Given(/^we send a checkstatus request for referential "([^"]+)"$/) do |referential|
+When(/^we send a checkstatus request for referential "([^"]+)"$/) do |referential|
   xmlBody = File.read("features/testdata/checkstatus-soap-request.xml")
   response = RestClient.post siri_path(referential: referential), xmlBody, {content_type: :xml}
   @last_siri_response = response.body
 end
 
-Then(/^we should recieve a positive checkstatus response$/) do
+Then(/^we should receive a positive checkstatus response$/) do
   xmlBody = @last_siri_response
   doc = REXML::Document.new xmlBody
   status = REXML::XPath.first(doc, "//*[local-name()='Status']")
