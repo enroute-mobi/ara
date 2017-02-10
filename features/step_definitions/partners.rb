a597f06fbda12c0faac3bb2ceef5417863b87c4a
Alban Peignier <alban.peignier@af83.com>
2017-02-10 12:12:58 +0100

    Update SIRI CheckStatus scenario. Refs #2553

---
+++ features/step_definitions/partners.rb
@@ -0,0 +1,8 @@
+def partners_path(attributes = {})
+  url_for_model(attributes.merge(resource: 'partner'))
+end
+
+Given(/^a Partner "([^"]*)" exists (?:in Referential "([^"]+)" )?with connectors \[([^"\]]*)\] and the following settings:$/) do |slug, referential, connectors, settings|
+	attributes = {"slug" => slug, "connectorTypes" => connectors.split(',').map(&:strip), "settings" => model_attributes(settings)}
+	RestClient.post partners_path(referential: referential), attributes.to_json, {content_type: :json, accept: :json}
+end
