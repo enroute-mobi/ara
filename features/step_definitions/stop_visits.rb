503a85fbc898f405a2d7a84b750a5deafe79751f
Alban Peignier <alban.peignier@af83.com>
2017-02-14 15:48:16 +0100

    Add scenario for Stop Visits API. Refs #2603

--- features/step_definitions/stop_visits.rb
+++ features/step_definitions/stop_visits.rb
@@ -2,23 +2,25 @@ def stop_visits_path(attributes = {})
   url_for_model(attributes.merge(resource: 'stop_visit'))
 end
 
-Given(/^a StopVisit exists (?:in Referential "([^"]+)" )?with the following attributes:$/) do |referential, stop_visit|
-  reponse = RestClient.post stop_visits_path(referential: referential), model_attributes(stop_visit).to_json, {content_type: :json}
-  # puts response.body
+def stop_visit_path(id, attributes = {})
+  url_for_model(attributes.merge(resource: 'stop_visit', id: id))
 end
 
-Then(/^one StopVisit has the following attributes:$/) do |attributes|
-	response = RestClient.get stop_visits_path
-	responseArray = JSON.parse(response.body)
+Given(/^a StopVisit exists (?:in Referential "([^"]+)" )?with the following attributes:$/) do |referential, stop_visit|
+  RestClient.post stop_visits_path(referential: referential), model_attributes(stop_visit).to_json, {content_type: :json}
+end
 
-	attributes = model_attributes(attributes)
+When(/^a StopVisit is created (?:in Referential "([^"]+)" )?with the following attributes:$/) do |referential, stopArea|
+  if referential.nil?
+    step "a StopVisit exists with the following attributes:", stopArea
+  else
+    step "a StopVisit exists in Referential \"#{referential}\" with the following attributes:", stopArea
+  end
+end
 
-	objectidkind = attributes["ObjectIds"].keys.first
-  objectid_value = attributes["ObjectIds"][objectidkind]
+Then(/^the StopVisit "([^"]*)" has the following attributes:$/) do |identifier, attributes|
+	response = RestClient.get stop_visit_path(identifier)
+	stopVisitAttributes = api_attributes(response.body)
 
-	expectedName = responseArray.find{|a| a["Name"] == attributes["Name"]}
-  expectedAttr = responseArray.find{|a| a["ObjectIDs"].find{|o| o["Kind"] == objectidkind && o["Value"] == objectid_value }}
-
-  expect(expectedName).not_to be_nil
-	expect(expectedAttr).not_to be_nil
+  expect(stopVisitAttributes).to include(model_attributes(attributes))
 end
--- features/support/attributes.rb
+++ features/support/attributes.rb
@@ -1,7 +1,31 @@
 def model_attributes(table)
   attributes = table.rows_hash.dup
 
-  attributes.each do |key, value|
+  attributes.dup.each do |key, value|
+    # Transform
+    #  | Schedule[aimed]#Arrival   | 2017-01-01T13:00:00.000Z          |
+    #  | Schedule[aimed]#Departure | 2017-01-01T13:02:00.000Z          |
+    # into
+    # "Schedules" => [
+    #   {"Kind"=>"aimed", "ArrivalTime"=>"2017-01-01T13:00:00.000Z", "DepartureTime"=>"2017-01-01T13:02:00.000Z"}
+    # ]
+    if key =~ /Schedule\[(aimed|expected|actual)\]#(Arrival|Departure)/
+      schedule_type = $1
+      attribute = $2
+
+      attributes["Schedules"] ||= []
+
+      schedule = attributes["Schedules"].find { |s| s["Kind"] == schedule_type }
+      unless schedule
+        schedule = { "Kind" => schedule_type }
+        attributes["Schedules"] << schedule
+      end
+
+      schedule["#{attribute}Time"] = value
+
+      attributes.delete key
+    end
+
     case value
     when /\A\d+\Z/
       # Convert integer
@@ -9,8 +33,20 @@ def model_attributes(table)
     end
   end
 
-  if attributes["ObjectIds"]
-    attributes["ObjectIds"] = JSON.parse("{#{attributes["ObjectIds"]}}")
+  if objectids = (attributes["ObjectIds"] || attributes["ObjectIDs"])
+    attributes["ObjectIDs"] = JSON.parse("{ #{objectids} }")
   end
+
+  attributes
+end
+
+def api_attributes(json)
+  attributes = JSON.parse(json)
+
+  objectids = attributes["ObjectIDs"]
+  if Array === objectids
+    attributes["ObjectIDs"] = Hash[objectids.map { |objectid| [objectid["Kind"], objectid["Value"]] }]
+  end
+
   attributes
 end
