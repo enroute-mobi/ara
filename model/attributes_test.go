a37e0e5428a468f7af0bfb51d6a3defaf73f9e44
Alban Peignier <alban.peignier@af83.com>
2017-03-19 15:04:52 +0100

    Create model.Attributes, with Set function. Refs #2873

---
+++ model/attributes_test.go
@@ -0,0 +1,38 @@
+package model
+
+import "testing"
+
+func Test_NewAttributes(t *testing.T) {
+	attributes := NewAttributes()
+
+	if len(attributes) != 0 {
+		t.Errorf("New attributes should be empty")
+	}
+}
+
+func Test_Attributes_Set(t *testing.T) {
+	attributes := NewAttributes()
+
+	attributes.Set("key", "value")
+
+	if len(attributes) != 1 {
+		t.Errorf("Attributes should have one entry")
+	}
+
+	if attributes["key"] != "value" {
+		t.Errorf("'key' should be associated to 'value'")
+	}
+}
+
+func Test_Attributes_Set_IgnoreEmptyValues(t *testing.T) {
+	attributes := NewAttributes()
+
+	attributes.Set("key", "")
+
+	if len(attributes) != 0 {
+		t.Errorf("Attributes should be empty")
+	}
+	if _, ok := attributes["key"]; ok {
+		t.Errorf("'key' should not be associated")
+	}
+}
