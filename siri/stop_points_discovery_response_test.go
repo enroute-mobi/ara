b6ff67dbe1cc9e0183af69e852053b95b9242b31
Alban Peignier <alban.peignier@af83.com>
2017-03-24 09:42:02 +0100

    Fixes StopPointsDiscoveryResponse StopName xml attribute. Refs #2886

--- siri/stop_points_discovery_response_test.go
+++ siri/stop_points_discovery_response_test.go
@@ -9,19 +9,19 @@ func Test_SIRIStopDiscoveryResponse_BuildXML(t *testing.T) {
 	expectedXML := `
 <ns8:StopPointsDiscoveryResponse xmlns:ns8="http://wsdl.siri.org.uk" xmlns:ns3="http://www.siri.org.uk/siri" xmlns:ns4="http://www.ifopt.org.uk/acsb" xmlns:ns5="http://www.ifopt.org.uk/ifopt" xmlns:ns6="http://datex2.eu/schema/2_0RC1/2_0" xmlns:ns7="http://scma/siri" xmlns:ns9="http://wsdl.siri.org.uk/siri">
    <Answer version="2.0">
-      <ns3:ResponseTimestamp>2016-09-21 20:14:46 +0000 UTC</ns3:ResponseTimestamp>
+      <ns3:ResponseTimestamp>2016-09-21T20:14:46.000Z</ns3:ResponseTimestamp>
       <ns3:Status>true</ns3:Status>
       <ns3:AnnotatedStopPointRef>
          <ns3:StopPointRef>NINOXE:StopPoint:BP:1:LOC</ns3:StopPointRef>
-         <ns3:StopPointName>Test 1</ns3:StopPointName>
+         <ns3:StopName>Test 1</ns3:StopName>
       </ns3:AnnotatedStopPointRef>
       <ns3:AnnotatedStopPointRef>
          <ns3:StopPointRef>NINOXE:StopPoint:BP:2:LOC</ns3:StopPointRef>
-         <ns3:StopPointName>Test 2</ns3:StopPointName>
+         <ns3:StopName>Test 2</ns3:StopName>
       </ns3:AnnotatedStopPointRef>
       <ns3:AnnotatedStopPointRef>
          <ns3:StopPointRef>NINOXE:StopPoint:BP:3:LOC</ns3:StopPointRef>
-         <ns3:StopPointName>Test 3</ns3:StopPointName>
+         <ns3:StopName>Test 3</ns3:StopName>
       </ns3:AnnotatedStopPointRef>
    </Answer>
    <AnswerExtension />
@@ -37,9 +37,9 @@ func Test_SIRIStopDiscoveryResponse_BuildXML(t *testing.T) {
 		ResponseTimestamp: responseTimestamp,
 	}
 
-	annoted1 := &SIRIAnnotatedStopPoint{StopPointRef: "NINOXE:StopPoint:BP:1:LOC", StopPointName: "Test 1"}
-	annoted2 := &SIRIAnnotatedStopPoint{StopPointRef: "NINOXE:StopPoint:BP:2:LOC", StopPointName: "Test 2"}
-	annoted3 := &SIRIAnnotatedStopPoint{StopPointRef: "NINOXE:StopPoint:BP:3:LOC", StopPointName: "Test 3"}
+	annoted1 := &SIRIAnnotatedStopPoint{StopPointRef: "NINOXE:StopPoint:BP:1:LOC", StopName: "Test 1"}
+	annoted2 := &SIRIAnnotatedStopPoint{StopPointRef: "NINOXE:StopPoint:BP:2:LOC", StopName: "Test 2"}
+	annoted3 := &SIRIAnnotatedStopPoint{StopPointRef: "NINOXE:StopPoint:BP:3:LOC", StopName: "Test 3"}
 
 	request.AnnotatedStopPoints = []*SIRIAnnotatedStopPoint{annoted1, annoted2, annoted3}
 
