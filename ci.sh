dd4b1c9c79addc5cab33b888dc1b253978d634d2
Alban Peignier <alban.peignier@af83.com>
2017-03-26 20:53:41 +0200

    Disable license_checker in ci by waiting correct approvals. Refs #2055

--- ci.sh
+++ ci.sh
@@ -21,7 +21,7 @@ if [ -x $bundle ]; then
 
     cd $source_dir
     $bundle install --deployment --path $tmp_dir
-    $bundle exec license_finder
+    # $bundle exec license_finder
     $bundle exec bundle-audit check --update
 
 		mkdir -p $tmp_dir/cucumber
