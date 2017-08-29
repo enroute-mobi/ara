38735f5c76acc3b7e88bce7e59b1b0d05d3857f3
Edouard Maffert <edouard.maffert@af83.com>
2017-08-29 11:49:57 +0200

    Remove unused method in Reference

--- model/reference.go
+++ model/reference.go
@@ -1,9 +1,6 @@
 package model
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import "encoding/json"
 
 type Reference struct {
 	ObjectId *ObjectID `json:",omitempty"`
@@ -17,26 +14,6 @@ func NewReference(objectId ObjectID) *Reference {
 
 func (reference *Reference) GetSha1() string {
 	return reference.ObjectId.HashValue()
-}
-
-func (reference *Reference) Getformat(ref, value string) string {
-	allRef := make(map[string]string)
-	loc := ""
-
-	if ref != "OperatorRef" {
-		loc = "LOC"
-	}
-	allRef["PlaceRef"] = "StopPoint:Q:"
-	allRef["OriginRef"] = "StopPoint:Q:"
-	allRef["DestinationRef"] = "StopPoint:Q:"
-	allRef["JourneyPatternRef"] = "JourneyPattern::"
-	allRef["RouteRef"] = "Route::"
-	allRef["DatedVehicleJourneyRef"] = "VehiculeJourney::"
-	allRef["OperatorRef"] = "Operator::"
-
-	formated := fmt.Sprintf("RATPDev:%s%s:%s", allRef[ref], value, loc)
-
-	return formated
 }
 
 func (reference *Reference) UnmarshalJSON(data []byte) error {
