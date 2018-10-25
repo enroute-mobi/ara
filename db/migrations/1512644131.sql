16566a49d54b4dd2bff42cbda59f031b353ec83d
Edouard Maffert <edouard.maffert@af83.com>
2018-10-25 18:38:22 +0200

    fix migration

--- db/migrations/1512644131.sql
+++ db/migrations/1512644131.sql
@@ -6,5 +6,5 @@ ALTER TABLE operators ADD model_name text NOT NULL;
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
-ALTER TABLE operators DROP model_name text NOT NULL;
+ALTER TABLE operators DROP model_name;
 
