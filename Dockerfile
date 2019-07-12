cb0c0b5500bbcdbdad8e246aaf7136a61dccac14
Alban Peignier <alban.peignier@af83.com>
2019-07-12 11:29:50 +0200

    Add Dockerfile for ci and run. Refs #12060

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Usage :
+# docker build -t ara .
+# docker run -it --add-host db:172.17.0.1 -e EDWIG_DB_NAME=edwig -e EDWIG_DB_USER=edwig -e EDWIG_DB_PASSWORD=edwig -e EDWIG_API_KEY=secret -e EDWIG_DEBUG=true -p 8080:8080 ara
+
+FROM golang:1.7 AS builder
+
+ENV DEV_PACKAGES="libxml2-dev"
+RUN apt-get update && apt-get -y install --no-install-recommends $DEV_PACKAGES
+
+WORKDIR /go/src/github.com/af83/edwig
+COPY . .
+
+ENV GO111MODULE=on
+RUN go get -d -v ./...
+RUN go install -v ./...
+
+FROM debian:latest
+
+ENV RUN_PACKAGES="libxml2"
+
+RUN apt-get update && apt-get -y dist-upgrade && apt-get -y install --no-install-recommends $RUN_PACKAGES && \
+    apt-get clean && apt-get -y autoremove && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+COPY --from=builder /go/bin/edwig ./
+ADD docker-entrypoint.sh ./
+COPY db/migrations ./db/migrations
+RUN chmod +x ./edwig ./docker-entrypoint.sh && mkdir ./config
+
+ENV EDWIG_CONFIG=./config EDWIG_ENV=production
+EXPOSE 8080
+
+ENTRYPOINT ["./docker-entrypoint.sh"]
+CMD ["api"]
--- ci.sh
+++ ci.sh
@@ -4,30 +4,30 @@ set -ex
 
 source_dir=`dirname $0`
 
-cp $source_dir/config/database-jenkins.yml $source_dir/config/database.yml
+cat > $source_dir/config/database.yml <<EOF
+test:
+  name: ${EDWIG_DB_NAME:-edwig_test}
+  user: ${EDWIG_DB_USER:-jenkins}
+  host: ${EDWIG_DB_HOST:-db}
+  password: ${EDWIG_DB_PASSWORD}
+  port: ${EDWIG_DB_PORT:-5432}
+EOF
 
+#(cd $source_dir; GO111MODULE=on go get -d -v ./...)
 go install github.com/af83/edwig
+
+PGHOST=${EDWIG_DB_HOST:-localhost} PGUSER=${EDWIG_DB_USER:-jenkins} PGPASSWORD=${EDWIG_DB_PASSWORD} createdb $EDWIG_DB_NAME
 
 export EDWIG_ENV=test
 (cd $source_dir; $GOPATH/bin/edwig migrate up)
 
 go test -v -cover github.com/af83/edwig/...
 
-ruby_bin_dir=`ls -d /var/lib/gems/*/bin | tail -1`
-bundle=$ruby_bin_dir/bundle
+tmp_dir=$GOPATH/tmp
 
-if [ -x $bundle ]; then
-		tmp_dir=$GOPATH/tmp
+cd $source_dir
+# $bundle exec license_finder
+bundle exec bundle-audit check --update
 
-    cd $source_dir
-    $bundle install --deployment --path $tmp_dir
-    # $bundle exec license_finder
-    $bundle exec bundle-audit check --update
-
-		mkdir -p $tmp_dir/cucumber
-    $bundle exec cucumber --tags "~@wip" --format json --out $tmp_dir/cucumber/cucumber.json --format html --out $tmp_dir/cucumber/index.html --format pretty --no-color
-
-    rm log/edwig.log
-else
-    echo "Bundle not detected, cucumber tests are skipped"
-fi
+mkdir -p $tmp_dir/cucumber
+bundle exec cucumber --tags "~@wip" --format json --out $tmp_dir/cucumber/cucumber.json --format html --out $tmp_dir/cucumber/index.html --format pretty --no-color
