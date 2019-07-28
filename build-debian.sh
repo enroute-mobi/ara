2389dbeedf9f4f53aec3f73c4ac183a233b6636d
Alban Peignier <alban.peignier@af83.com>
2019-07-28 17:49:21 +0200

    Add BUILD_NUMBER in Debian version when available. Refs #12158

--- build-debian.sh
+++ build-debian.sh
@@ -4,6 +4,11 @@ set -e
 
 TARGET_DIR=${1:-target}
 
+if [ -n "$BUILD_NUMBER" ]; then
+    debian_release=`dpkg-parsechangelog --count 1 | sed -n '/^Version: / s/Version: //p'`
+    dch --changelog debian/changelog --newversion "${debian_release}+${deploy_env}${BUILD_NUMBER}" --distribution unstable 'New build'
+fi
+
 mk-build-deps -i -t 'apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y'
 debuild -us -uc
 
