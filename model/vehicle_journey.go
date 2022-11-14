dadb442ecbce332c00e2ace3207b4114ea27b77e
renaud <renaud.vignery@enroute.mobi>
2022-11-14 14:53:17 +0100

    ARA-1101 remove unused constants

--- model/vehicle_journey.go
+++ model/vehicle_journey.go
@@ -11,8 +11,6 @@ import (
 const (
 	VEHICLE_DIRECTION_INBOUND  = "inbound"
 	VEHICLE_DIRECTION_OUTBOUND = "outbound"
-	VEHICLE_DIRECTION_ALLER    = "aller"
-	VEHICLE_DIRECTION_RETOUR   = "retour"
 )
 
 type VehicleJourneyId ModelId
