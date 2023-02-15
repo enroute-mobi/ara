0a0cc2a1576fe3c30e433921b19659c611bfd42a
Alban Peignier <alban.peignier@enroute.mobi>
2023-02-15 20:49:57 +0100

    ARA-1250 Use deploy-with-runner.sh script

--- script/bitbucket-gcloud-configure-docker.sh
+++ script/bitbucket-gcloud-configure-docker.sh
@@ -1,6 +1,12 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-echo "$GCLOUD_API_KEYFILE" | base64 -d > ~/.gcloud-api-key.json
+# Manage base64 encoded or raw GCLOUD_API_KEYFILE
+if echo "$GCLOUD_API_KEYFILE" | grep -q "private_key"; then
+    echo -En "$GCLOUD_API_KEYFILE" > ~/.gcloud-api-key.json
+else
+    echo "$GCLOUD_API_KEYFILE" | base64 -d > ~/.gcloud-api-key.json
+fi
+
 gcloud auth activate-service-account --key-file ~/.gcloud-api-key.json
 gcloud config set project "$GCLOUD_PROJECT"
 gcloud auth configure-docker --quiet
