c6b722b7ce3af5661fc560152b9457e7fb7804de
renaud <renaud.vignery@enroute.mobi>
2023-03-21 17:30:25 +0100

    ARA-1232 rename structures for coherence

--- siri/siri/vehicle_monitoring_response.go
+++ siri/siri/vehicle_monitoring_response.go
@@ -4,68 +4,68 @@ import (
 	"time"
 )
 
-type VehicleMonitoringDelivery struct {
+type SIRIVehicleMonitoringDelivery struct {
 	Version           string
 	ResponseTimestamp time.Time `json:",omitempty"`
 	RequestMessageRef string    `json:",omitempty"`
 	Status            bool
 	ErrorCondition    *ErrorCondition `json:",omitempty"`
-	VehicleActivity   []*VehicleActivity
+	VehicleActivity   []*SIRIVehicleActivity
 }
 
-type VehicleActivity struct {
-	RecordedAtTime          time.Time                `json:",omitempty"`
-	ValidUntilTime          time.Time                `json:",omitempty"`
-	VehicleMonitoringRef    string                   `json:",omitempty"`
-	MonitoredVehicleJourney *MonitoredVehicleJourney `json:",omitempty"`
+type SIRIVehicleActivity struct {
+	RecordedAtTime          time.Time                    `json:",omitempty"`
+	ValidUntilTime          time.Time                    `json:",omitempty"`
+	VehicleMonitoringRef    string                       `json:",omitempty"`
+	MonitoredVehicleJourney *SIRIMonitoredVehicleJourney `json:",omitempty"`
 }
 
-type MonitoredVehicleJourney struct {
-	LineRef                 string                   `json:",omitempty"`
-	FramedVehicleJourneyRef *FramedVehicleJourneyRef `json:",omitempty"`
-	PublishedLineName       string                   `json:",omitempty"`
-	DirectionName           string                   `json:",omitempty"`
-	OriginRef               string                   `json:",omitempty"`
-	OriginName              string                   `json:",omitempty"`
-	DestinationRef          string                   `json:",omitempty"`
-	DestinationName         string                   `json:",omitempty"`
+type SIRIMonitoredVehicleJourney struct {
+	LineRef                 string                       `json:",omitempty"`
+	FramedVehicleJourneyRef *SIRIFramedVehicleJourneyRef `json:",omitempty"`
+	PublishedLineName       string                       `json:",omitempty"`
+	DirectionName           string                       `json:",omitempty"`
+	OriginRef               string                       `json:",omitempty"`
+	OriginName              string                       `json:",omitempty"`
+	DestinationRef          string                       `json:",omitempty"`
+	DestinationName         string                       `json:",omitempty"`
 	Monitored               bool
 	Delay                   *time.Time `json:",omitempty"`
 	Bearing                 float64
-	VehicleLocation         *VehicleLocation
+	VehicleLocation         *SIRIVehicleLocation
 	Occupancy               string `json:",omitempty"`
 	DriverRef               string `json:",omitempty"`
 }
 
-type FramedVehicleJourneyRef struct {
+type SIRIFramedVehicleJourneyRef struct {
 	DataFrameRef           string
 	DatedVehicleJourneyRef string
 }
 
-type VehicleLocation struct {
+type SIRIVehicleLocation struct {
 	Coordinates string  `json:",omitempty"`
 	Longitude   float64 `json:",omitempty"`
 	Latitude    float64 `json:",omitempty"`
 }
 
-func NewSiriLiteVehicleMonitoringDelivery() *VehicleMonitoringDelivery {
-	return &VehicleMonitoringDelivery{
+func NewSiriLiteVehicleMonitoringDelivery() *SIRIVehicleMonitoringDelivery {
+	return &SIRIVehicleMonitoringDelivery{
 		Version:         "2.0:FR-IDF-2.4",
-		VehicleActivity: []*VehicleActivity{},
+		VehicleActivity: []*SIRIVehicleActivity{},
 	}
 }
 
-func NewSiriLiteVehicleActivity() *VehicleActivity {
-	mvj := &MonitoredVehicleJourney{
-		VehicleLocation:         &VehicleLocation{},
-		FramedVehicleJourneyRef: &FramedVehicleJourneyRef{},
+func NewSiriLiteVehicleActivity() *SIRIVehicleActivity {
+	mvj := &SIRIMonitoredVehicleJourney{
+		VehicleLocation:         &SIRIVehicleLocation{},
+		FramedVehicleJourneyRef: &SIRIFramedVehicleJourneyRef{},
 	}
-	return &VehicleActivity{
+	return &SIRIVehicleActivity{
 		MonitoredVehicleJourney: mvj,
 	}
 }
 
-type VehicleActivities []*VehicleActivity
+type VehicleActivities []*SIRIVehicleActivity
 
 func (a VehicleActivities) Len() int      { return len(a) }
 func (a VehicleActivities) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
