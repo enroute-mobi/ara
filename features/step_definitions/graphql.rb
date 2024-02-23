1222513fcb6cd3b4c09de91696b0cad12a5edb94
Edouard Maffert <edouard.maffert@enroute.mobi>
2024-02-23 15:01:44 +0100

    ARA-1439 Small fix in cucumber specs

--- features/step_definitions/graphql.rb
+++ features/step_definitions/graphql.rb
@@ -1,44 +1,44 @@
-module GraphQlHelper
+# module GraphQlHelper
 
-  def a_graphql_resource_having_attributes(attributes)
-    attributes = attributes.rows_hash if attributes.respond_to?(:rows_hash)
+#   def a_graphql_resource_having_attributes(attributes)
+#     attributes = attributes.rows_hash if attributes.respond_to?(:rows_hash)
 
-    attributes_with_human_names = attributes
-    attributes = {}
+#     attributes_with_human_names = attributes
+#     attributes = {}
 
-    attributes_with_human_names.each do |human_name, value|
-      graphql_name = human_name.parameterize(separator: '_').camelize(:lower)
-      attributes[graphql_name] = value
-    end
+#     attributes_with_human_names.each do |human_name, value|
+#       graphql_name = human_name.parameterize(separator: '_').camelize(:lower)
+#       attributes[graphql_name] = value
+#     end
 
-    attribute_matchers = attributes.map do |attribute, value|
-      matcher =
-        case value
-        when %r{^"(.*)"$}
-          $1
-        when %r{^/(.*)/$}
-          match(Regexp.new($1))
-        when %r{^-?\d+$}
-          value.to_i
-        when %r{^-?\d+\.\d+$}
-          a_value_within(0.001).of(value.to_f)
-        when "true"
-          be_truthy
-        when "false"
-          be_falsy
-        when %r{^{.*}$}
-          JSON.parse(value)
-        else
-          eq(value)
-        end
-      [ attribute, matcher ]
-    end.to_h
+#     attribute_matchers = attributes.map do |attribute, value|
+#       matcher =
+#         case value
+#         when %r{^"(.*)"$}
+#           $1
+#         when %r{^/(.*)/$}
+#           match(Regexp.new($1))
+#         when %r{^-?\d+$}
+#           value.to_i
+#         when %r{^-?\d+\.\d+$}
+#           a_value_within(0.001).of(value.to_f)
+#         when "true"
+#           be_truthy
+#         when "false"
+#           be_falsy
+#         when %r{^{.*}$}
+#           JSON.parse(value)
+#         else
+#           eq(value)
+#         end
+#       [ attribute, matcher ]
+#     end.to_h
 
-    a_hash_including(attribute_matchers)
-  end
-end
+#     a_hash_including(attribute_matchers)
+#   end
+# end
 
-World(GraphQlHelper)
+# World(GraphQlHelper)
 
 When('I send this GraphQL query to the Referential {string} with token {string}') do |referential, partner_token, query|
   url = URI(url_for referential: referential, path: "graphql")
@@ -64,7 +64,6 @@ end
 
 Then('the GraphQL response should contain a Vehicle with these attributes:') do |attributes|
   collection = @graphql_response["data"]["vehicles"]
-  matcher = a_graphql_resource_having_attributes(attributes)
 
   expect(collection).to include(model_attributes attributes)
 end
