25b06ef731c4d9f1bd36065f098d95302c2ce671
renaud <renaud.vignery@enroute.mobi>
2025-05-20 18:09:49 +0200

    ARA-1711 use ara-ruby for Line Group step definition

--- features/line_groups.feature
+++ features/line_groups.feature
@@ -8,8 +8,7 @@ Feature: Manager Line Groups
       | Name      | Test       |
       | ShortName | short_name |
       | LineIds   | ["1234"]   |
-    Then one Line Group has the following attributes:
-      | Id        | 6ba7b814-9dad-11d1-1-00c04fd430c8 |
+    Then the Line Group "6ba7b814-9dad-11d1-1-00c04fd430c8" has the following attributes:
       | Name      | Test                              |
       | ShortName | short_name                        |
       | LineIds   | ["1234"]                          |
--- features/step_definitions/line_groups.rb
+++ features/step_definitions/line_groups.rb
@@ -1,28 +1,21 @@
-def line_groups_path(attributes = {})
-  url_for_model(attributes.merge(resource: 'line_group'))
+Given(/^a Line Group exists (?:in Referential "([^"]+)" )?with the following attributes:$/) do |slug, line_group|
+  referential = find_referential(slug)
+  line_group = referential.line_groups.create(model_attributes(line_group).transform_keys { |key| key.to_s.underscore })
+
+  raise 'Cannot create line group' unless line_group.save
 end
 
-def line_group_path(id, attributes = {})
-  ufrl_for_model(attributes.merge(resource: 'line_groups', id: id))
-end
-
-Given(/^a Line Group exists (?:in Referential "([^"]+)" )?with the following attributes:$/) do |referential, lineGroup|
-  response = RestClient.post line_groups_path(referential: referential), model_attributes(lineGroup).to_json, {content_type: :json, :Authorization => "Token token=#{$token}"}
-  debug response.body
-end
-
-
-When(/^a Line Group is created (?:in Referential "([^"]+)" )?with the following attributes:$/) do |referential, lineGroup|
+When(/^a Line Group is created (?:in Referential "([^"]+)" )?with the following attributes:$/) do |referential, line_group|
   if referential.nil?
-    step "a Line Group exists with the following attributes:", lineGroup
+    step "a Line Group exists with the following attributes:", line_group
   else
-    step "a Line Group exists in Referential \"#{referential}\" with the following attributes:", lineGroup
+    step "a Line Group exists in Referential \"#{referential}\" with the following attributes:", line_group
   end
 end
 
-Then(/^one Line Group(?: in Referential "([^"]+)")? has the following attributes:$/) do |referential, attributes|
-  response = RestClient.get line_groups_path(referential: referential), {content_type: :json, :Authorization => "Token token=#{$token}"}
-  response_array = api_attributes(response.body)
+Then(/^the Line Group "([^"]*)" has the following attributes:$/) do |identifier, attributes|
+  line_group = find_model(nil, :line_groups, identifier)
+  expect(line_group).not_to be_nil
 
-  expect(response_array).to include(model_attributes(attributes))
+  matcher_attributes(attributes, line_group)
 end
