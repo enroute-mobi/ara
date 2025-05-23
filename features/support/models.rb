e36f3666ba5b3979cd803bdfdb7166187b24d158
renaud <renaud.vignery@enroute.mobi>
2025-05-23 12:54:15 +0200

    ARA-1711 refacto: use ParameterType for StopArea

--- features/support/models.rb
+++ features/support/models.rb
@@ -15,3 +15,60 @@ def referential_models(slug, model)
   referential = find_referential(slug)
   referential.send(model).all
 end
+
+ParameterType(
+  name: 'ara_resource',
+  regexp: Regexp.new(
+    %w[StopArea].join('|')
+  ),
+  transformer: ->(s) { s }
+)
+
+Given("a {ara_resource} is created with the following attributes:") do |collection, attributes|
+  step "a #{collection} is created in Referential \"test\" with the following attributes:", attributes
+end
+
+Given("a {ara_resource} is created in Referential {string} with the following attributes:") do |collection, slug, attributes|
+  referential = find_referential(slug)
+  model = referential.send("#{collection.underscore}s").create(model_attributes(attributes).transform_keys { |key| key.to_s.underscore.to_sym })
+
+  raise "Cannot create #{collection}: #{model.errors}" unless model.save
+end
+
+When("a {ara_resource} exists with the following attributes:") do |collection, attributes|
+  step "a #{collection} is created with the following attributes:", attributes
+end
+
+Then("one {ara_resource} has the following attributes:") do |collection, attributes|
+  step "one #{collection} in Referential \"test\" has the following attributes:", attributes
+end
+
+Then("one {ara_resource} in Referential {string} has the following attributes:") do |collection, slug, attributes|
+  referential = find_referential(slug)
+
+  check_attributes(referential, "#{collection.underscore}s", attributes)
+end
+
+When('the {ara_resource} {string}:{string} is destroyed') do |ara_resource, code_space, value|
+  model = find_model('test', "#{ara_resource.underscore}s", "#{code_space}:#{value}")
+
+  raise "Cannot destroy #{ara_model}: #{model.errors}" unless model.destroy
+end
+
+Then("a {ara_resource} {string}:{string} should exist") do |collection, code_space, value|
+  step "a #{collection} \"#{code_space}\":\"#{value}\" should exist in Referential \"test\""
+end
+
+Then("a {ara_resource} {string}:{string} should exist in Referential {string}") do |collection, code_space, value, slug|
+  model = find_model(slug, "#{collection.underscore}s", "#{code_space}:#{value}")
+  expect(model).not_to be_nil
+end
+
+Then("a {ara_resource} {string}:{string} should not exist") do |collection, code_space, value|
+  step "a #{collection} \"#{code_space}\":\"#{value}\" should not exist in Referential \"test\""
+end
+
+Then("a {ara_resource} {string}:{string} should not exist in Referential {string}") do |collection, code_space, value, slug|
+  model = find_model(slug, "#{collection.underscore}s", "#{code_space}:#{value}")
+  expect(model).to be_nil
+end
